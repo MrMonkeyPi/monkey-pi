8ed417c6f0eab322c438636fcf7ae7e733bb0939
HansHuang <hys0550@sina.com>
2022-03-20 23:49:46 +0800

    start function

---
+++ src/service/robot/walk.py
@@ -0,0 +1,91 @@
+import RPi.GPIO as GPIO
+import time
+
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
+
+def motor_init():
+    global pwm_ENA
+    global pwm_ENB
+    global delaytime
+    GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+
+    pwm_ENA = GPIO.PWM(ENA, 2000)
+    pwm_ENB = GPIO.PWM(ENB, 2000)
+    pwm_ENA.start(0)
+    pwm_ENB.start(0)
+
+def forward():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def back():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def left():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def right():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def spin_left():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def spin_right():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+def stop():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(80)
+    pwm_ENB.ChangeDutyCycle(80)
+
+
+def motor_clean():
+    pwm_ENA.stop()
+    pwm_ENB.stop()
+    GPIO.cleanup() 
--- src/ui/preact/routes/home/index.js
+++ src/ui/preact/routes/home/index.js
@@ -1,12 +1,52 @@
 import { h } from 'preact';
 import style from './style.css';
+import 'css.gg/icons/css/arrow-up.css'
+import 'css.gg/icons/css/arrow-left.css'
+import 'css.gg/icons/css/play-stop.css'
+import 'css.gg/icons/css/arrow-right.css'
+import 'css.gg/icons/css/arrow-down.css'
+
+import 'css.gg/icons/css/chevron-up.css'
+import 'css.gg/icons/css/chevron-left.css'
+import 'css.gg/icons/css/edit-shadows.css'
+import 'css.gg/icons/css/chevron-right.css'
+import 'css.gg/icons/css/chevron-down.css'
+
+import 'css.gg/icons/css/airplane.css'
 
 const Home = () => (
 	<section id="home" class="d-flex flex-column justify-content-center">
-		<div class="container" data-aos="zoom-in" data-aos-delay="100">
-			<h1>Mr. Monkey Pi</h1>
-			<p>I'm <span class="typed" data-typed-items="Developer, Freelancer"></span></p>
+		<div class="container">
+			<div>lass="text-start
+				<p>I'm <span class="typed" data-typed-items="Developer, Assistant"></span></p>
+				<h1>Mr. Monkey Pi</h1>
+			</div>
 
+			<div class="position-fixed bottom-0 start-0 end-0 p-3">
+				<div class="d-flex justify-content-between">
+					<div>
+						<p class="text-center"><button type="button" class="btn btn-outline-secondary"><i class="gg-arrow-up"></i></button></p>
+						<p class="text-center">
+							<button type="button" class="btn btn-outline-secondary"><i class="gg-arrow-left"></i></button>
+							<button type="button" class="btn btn-outline-secondary p-2 mx-4" ><i class="gg-play-stop"></i></button>
+							<button type="button" class="btn btn-outline-secondary"><i class="gg-arrow-right"></i></button>
+						</p>
+						<p class="text-center"><button type="button" class="btn btn-outline-secondary"><i class="gg-arrow-down"></i></button></p>
+					</div>
+					<div class="d-flex align-items-end">
+						<p class="text-center"><button type="button" class="btn btn-outline-secondary"><i class="gg-airplane"></i></button></p>
+					</div>
+					<div>
+						<p class="text-center"><button type="button" class="btn btn-outline-secondary"><i class="gg-chevron-up"></i></button></p>
+						<p class="text-center">
+							<button type="button" class="btn btn-outline-secondary"><i class="gg-chevron-left"></i></button>
+							<button type="button" class="btn btn-outline-secondary p-2 mx-4" ><i class="gg-edit-shadows"></i></button>
+							<button type="button" class="btn btn-outline-secondary"><i class="gg-chevron-right"></i></button>
+						</p>
+						<p class="text-center"><button type="button" class="btn btn-outline-secondary"><i class="gg-chevron-down"></i></button></p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 );
